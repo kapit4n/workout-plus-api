8a8ca9b02f8917cb7360585bd62c91f4b36f94f8
Luis Arce <luis.arce22@gmail.com>
2022-03-03 21:54:44 -0400

    Adding cors

--- server.go
+++ server.go
@@ -6,13 +6,20 @@ import (
 	"workoutplus/db"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	db := db.Connect()
 
